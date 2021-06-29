98b1b6b5edfeb8cdd33f320dd7033a62594c1ee8
ag91 <agiugliano@live.it>
2021-06-29 17:18:46 +0100

    autoconfig for nyxt 2.1.0

--- auto-config.lisp
+++ auto-config.lisp
@@ -1,8 +1,8 @@
-(define-configuration (buffer web-buffer)
-    ((default-modes (append '(emacs-mode) %slot-default))))
 (define-configuration buffer
-    ((current-zoom-ratio 4)))
+  ((current-zoom-ratio 4)))
 (define-configuration browser
-    ((external-editor-program "/usr/bin/emacsclient")))
+  ((external-editor-program '("/usr/bin/emacsclient"))))
 (define-configuration browser
-    ((session-restore-prompt :never-restore)))
+  ((session-restore-prompt :never-restore)))
+(define-configuration buffer
+  ((default-modes (append '(nyxt::emacs-mode) %slot-default%))))
