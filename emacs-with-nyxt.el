ca090006f69d06753ad4e5ee69da0966a0c744e9
ag91 <agiugliano@live.it>
2021-07-08 15:50:33 +0100

    add REPL eval with M-: and org capture commands

--- emacs-with-nyxt.el
+++ emacs-with-nyxt.el
@@ -48,6 +48,12 @@
         (slime-repl-eval-string s-exps-string)
       (error "Slime is not connected to Nyxt. Run `emacs-with-nyxt-start-and-connect-to-nyxt' first"))))
 
+(add-to-list
+ 'org-capture-templates
+ `("wN" "Web link" entry (file+headline ,(car org-agenda-files) "Links to read later")
+   "* %?%a :readings: \nSCHEDULED: %(org-insert-time-stamp (org-read-date nil t \"Fri\"))\n"
+   :immediate-finish t :empty-lines 2))
+
 (defun emacs-with-nyxt-start-and-connect-to-nyxt (&optional no-maximize)
   "Start Nyxt with swank capabilities. Optionally skip window maximization with NO-MAXIMIZE."
   (interactive)
@@ -92,6 +98,41 @@
       (when (equal (mode-name (current-buffer)) 'web-buffer))
       (eval-in-emacs
        `(progn (switch-to-buffer (get-buffer-create ,(render-url (url (current-buffer))))) (erase-buffer) (insert ,(ffi-buffer-get-document (current-buffer))) (html-mode) (indent-region (point-min) (point-max)))))
+
+   ;; from @aartaka https://www.reddit.com/r/Nyxt/comments/ock3tu/is_there_something_like_mx_or_esc_in_nyxt/h3wkipl?utm_source=share&utm_medium=web2x&context=3
+   `(define-command-global eval-expression ()
+      "Prompt for the expression and evaluate it, echoing result to the `message-area'."
+      (let ((expression-string
+             ;; Read an arbitrary expression. No error checking, though.
+             (first (prompt :prompt "Expression to evaluate"
+                            :sources (list (make-instance 'prompter:raw-source))))))
+        ;; Message the thing to the message-area down below.
+        (echo "~S" (eval (read-from-string expression-string)))))
+
+   `(define-configuration nyxt/web-mode:web-mode
+      ;; Bind eval-expression to M-:, but only in emacs-mode.
+      ((keymap-scheme (let ((scheme %slot-default%))
+                        (keymap:define-key (gethash scheme:emacs scheme)
+                                           "M-:" 'eval-expression)
+                        scheme))))
+   `(define-command-global org-capture ()
+      "Org-capture current page."
+      (eval-in-emacs
+       `(org-link-set-parameters
+         "nyxt"
+         :store (lambda ()
+                  (org-store-link-props
+                   :type "nyxt"
+                   :link ,(quri:render-uri (url (current-buffer)))
+                   :description ,(title (current-buffer)))))
+       `(org-capture nil "wN"))
+      (echo "Note stored!"))
+   `(define-configuration nyxt/web-mode:web-mode
+      ;; Bind org-capture to C-o-c, but only in emacs-mode.
+      ((keymap-scheme (let ((scheme %slot-default%))
+                        (keymap:define-key (gethash scheme:emacs scheme)
+                                           "C-c o c" 'org-capture)
+                        scheme))))
    )
   (unless no-maximize
     (emacs-with-nyxt-slime-repl-send-sexps
