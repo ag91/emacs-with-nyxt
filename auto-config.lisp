5c5b762d22198e74cf52c6b688b6eaf254c68236
ag91 <agiugliano@live.it>
2021-10-24 16:14:11 +0100

    try to use 0.0.0.0 instead of localhost for slynk connection (for MacOS integration)

--- auto-config.lisp
+++ auto-config.lisp
@@ -36,13 +36,13 @@
 (load "~/quicklisp/setup.lisp")
 (ql:quickload :slynk)
 (define-command-global start-slynk (&optional (slynk-port *swank-port*))
-    "Start a Slynk server that can be connected to, for instance, in
+  "Start a Slynk server that can be connected to, for instance, in
 Emacs via SLY.
 
 Warning: This allows Nyxt to be controlled remotely, that is, to execute
 arbitrary code with the privileges of the user running Nyxt.  Make sure
 you understand the security risks associated with this before running
 this command."
-    (slynk:create-server :port slynk-port :dont-close t)
-    (echo "Slynk server started at port ~a" slynk-port))
+  (slynk:create-server :port slynk-port :dont-close t :interface "0.0.0.0")
+  (echo "Slynk server started at port ~a" slynk-port))
 (start-slynk)
